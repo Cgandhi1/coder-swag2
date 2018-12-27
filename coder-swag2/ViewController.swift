10d4be2f59a6741aab28a96cbd6ef3733cb37523
admin <admin.@admins-macbook-pro.local>
2018-12-27 17:59:34 -0600

    Initial Commit

---
+++ coder-swag2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag2
+//
+//  Created by  admin on 12/27/18.
+//  Copyright © 2018  admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
